3b45f0bdc4893ee0b4eb92e7f8b845973a727823
Todd Koehnen <todd.koehnen@stratasys.com>
2023-01-18 16:44:32 +0000

    Restructure model sql files in the models folder for training

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -6,24 +6,13 @@
 
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
@@ -31,13 +20,11 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-
     from orders
-
+    
     group by 1
 
 ),
@@ -59,4 +46,4 @@ final as (
 
 )
 
-select * from final
+select * from final;
