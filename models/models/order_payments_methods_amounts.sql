d2408890c7067f1e186daa5fa307cf6a9b676164
Todd Koehnen <todd.koehnen@stratasys.com>
2023-01-18 14:58:43 +0000

    Commit changes

---
+++ models/models/order_payments_methods_amounts.sql
@@ -0,0 +1,94 @@
+--The following is repetitive and can be difficult to maintain
+select
+order_id,
+sum(case when payment_method = 'bank_transfer' then amount end) as bank_transfer_amount,
+sum(case when payment_method = 'credit_card' then amount end) as credit_card_amount,
+sum(case when payment_method = 'gift_card' then amount end) as gift_card_amount,
+sum(amount) as total_amount
+from {{ ref('raw_payments') }}
+group by 1;
+
+
+
+-- Use a for loop in models for repeated SQL
+  -- The following will be compiled to the same query, but is significantly easier to maintain.
+select
+order_id,
+{% for payment_method in ["bank_transfer", "credit_card", "gift_card"] %}
+sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
+{% endfor %}
+sum(amount) as total_amount
+from {{ ref('raw_payments') }}
+group by 1;
+
+
+
+-- Setting variables at the top of a model helps with readability, and enables you to reference the list in multiple places if required
+{% set payment_methods = ["bank_transfer", "credit_card", "gift_card"] %}
+
+select
+order_id,
+{% for payment_method in payment_methods %}
+sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
+{% endfor %}
+sum(amount) as total_amount
+from {{ ref('raw_payments') }}
+group by 1;
+
+
+
+-- Use loop.last to avoid trailing commas ()
+{% set payment_methods = ["bank_transfer", "credit_card", "gift_card"] %}
+
+select
+order_id,
+{% for payment_method in payment_methods %}
+sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount
+{% if not loop.last %},{% endif %}
+{% endfor %}
+from {{ ref('raw_payments') }}
+group by 1;
+
+
+
+--Use whitespace control to tidy up compiled code
+  -- Model code readability is more important than compiled code, so use this only as an extra polish (often trial and error)
+{%- set payment_methods = ["bank_transfer", "credit_card", "gift_card"] -%}
+
+select
+order_id,
+{%- for payment_method in payment_methods %}
+sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount
+{%- if not loop.last %},{% endif -%}
+{% endfor %}
+from {{ ref('raw_payments') }}
+group by 1;
+
+
+
+-- Use a macro to return payment methods
+  -- Note that we didn't use curly braces when calling the macro – we're already within a Jinja statement, so there's no need to use the brackets again.
+{%- set payment_methods = get_payment_methods() -%}
+
+select
+order_id,
+{%- for payment_method in payment_methods %}
+sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount
+{%- if not loop.last %},{% endif -%}
+{% endfor %}
+from {{ ref('raw_payments') }}
+group by 1;
+
+
+
+--Dynamically retrieve the list of payment methods
+select distinct
+payment_method
+from {{ ref('raw_payments') }}
+order by 1;
+
+
+
+--
+
+
