d6c376bf2f6d213a4333199d25fe833271c92a11
Todd Koehnen <todd.koehnen@stratasys.com>
2023-01-26 21:43:34 +0000

    Commit within macro training

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,20 +3,18 @@
 with source as (
     select * from {{ source('stripe', 'payment') }}
 ),
-
 staged as (
-
     select 
         id as payment_id,
         orderid as order_id,
         paymentmethod as payment_method,
         status as payment_status,
+        amount as amount_cents,
         -- convert amount stored in 'cents' to 'dollars'
-        amount / 100 as amount,
+        round(amount / 100, 4) as amount_usd,
+        {{ cents_to_dollars('amount', 4) }} as amount,
         created as payment_created_date,
         _batched_at
     from source
-
 )
-
 select * from staged
