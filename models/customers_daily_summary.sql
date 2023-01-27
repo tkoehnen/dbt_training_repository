12ea573fa026cd3b30d197def831cdecf8d95af7
Todd Koehnen <todd.koehnen@stratasys.com>
2023-01-27 15:57:27 +0000

    Commit after packages with macros training

--- models/customers_daily_summary.sql
+++ models/customers_daily_summary.sql
@@ -1,35 +1,7 @@
-/*
-select
-    customer_id,
-    order_date,
-    count(*)
-from {{ ref('stg_orders') }}
-group by 1,2
-*/
-
---Add surrogate key column using (surrogate_key) macro with specific columns to return unique hash id:
---/*
-select
-    {{ dbt_utils.surrogate_key(['customer_id', 'order_date']) }} as id,
-    customer_id,
-    order_date,
-    count(*)
-from {{ ref('stg_orders') }}
-group by 1,2,3
---*/
-/*
-Warning: `dbt_utils.surrogate_key` has been replaced by `dbt_utils.generate_surrogate_key`. 
-The new macro treats null values differently to empty strings. 
-To restore the behaviour of the original macro, 
-    add a global variable in dbt_project.yml called `surrogate_key_treat_nulls_as_empty_strings` to your dbt_project.yml file with a value of True. 
-The jaffle_shop.customers_daily_summary model triggered this warning.
-*/
-/*
 select
     {{ dbt_utils.generate_surrogate_key(['customer_id', 'order_date']) }} as id,
     customer_id,
     order_date,
-    count(*)
+    count(order_id) as order_count
 from {{ ref('stg_orders') }}
 group by 1,2,3
-*/
